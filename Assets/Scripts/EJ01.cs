ad67dda2801431e9fa4899d23b3e60c3b0d09d19
SaniagoAparicio <101577286+SaniagoAparicio@users.noreply.github.com>
2022-03-29 10:40:35 -0300

    Added_EJERCICIO_01

--- Assets/Scripts/EJ01.cs
+++ Assets/Scripts/EJ01.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class EJ01 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   // 1. Realizá un programa donde se cree una variable entera llamada num1, que inicialmente
+   // valdrá 0. Luego incrementá su valor en 2 y mostralo por pantalla.Después mostrá el
+   // resultado de multiplicarlo por sí mismo.
+
+        void Start()
     {
-        
+        int num1 = 0;
+        Debug.Log(num1 += 2);
+        Debug.Log(num1 * num1);
     }
 
     // Update is called once per frame
